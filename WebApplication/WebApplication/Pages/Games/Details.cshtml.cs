3eb6aa98333ec711c27fb87a0a0fb92fb5fc9a1e
bkempa <barkem01@gmail.com>
2023-04-16 13:59:03 +0200

    Initial commit

---
+++ WebApplication/WebApplication/Pages/Games/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+using WebApplication.Models;
+
+namespace WebApplication.Pages.Games
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WebApplication.Data.WebApplicationContext _context;
+
+        public DetailsModel(WebApplication.Data.WebApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public Game Game { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Game = await _context.Game.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Game == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
